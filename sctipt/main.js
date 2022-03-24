bd8b44f4f9064bc4cd3e5bef53c2913e748dac33
Martell-hub <76472889+Martell-hub@users.noreply.github.com>
2022-03-24 21:06:23 +0200

    Add files via upload

--- sctipt/main.js
+++ sctipt/main.js
@@ -1,24 +1,18 @@
-const FLAG = '•', FLAG_IMG = ''
-const LOSS = '☺', LOSS_IMG = ''
-const MINE = '♣', MINE_IMG = ''
-const SMILY = '☻', SMILY_IMG = ''
-const WIN = '♥', WIN_IMG = ''
+const FLAG = '•'
+const LOSS = '☺'
+const MINE = '♣'
+const SMILY = '☻'
+const WIN = '♥'
 
 var gFlag = 0
 var gLive = 3
 var gHint = 3
 var gBoard
 
-var gCell = {
-  minesAroundCount: 0,
-  isShown: false,
-  isMine: false,
-  isMarked: false,
-}
 
 var gLevel = {
   SIZE: 4,
-  MINES: 4,
+  MINES: 0,
 }
 var gGame = {
   isOn: false,
@@ -28,91 +22,58 @@ var gGame = {
 }
 
 function init() {
-  gBoard = buildBoard()
+  gBoard = createBoard()
   renderBoard(gBoard)
 }
 
 
+function cellClicked(elCell) {
+  elCell.classList.remove('hiden')
+  getSelectedElCell = elCell
+  var cellCoords = getSelectedCoord(elCell.id)
+  console.log(cellCoords);
+  //isFirstMove()
+}
+function getSelectedCoord(strCellId){
+  var coord = {}
+  var parts = strCellId.split('-')
+  coord.i = +parts[1]
+  coord.j = +parts[2]
+  return coord
+}
 
+function cellMarked(elCell) { }
 
-////--------------------------------------------------------------------------
-// function setMinesNegsCount(board) {
-//   console.log(board);
-//   var neighborsCount = 0;
-//   for (var i = l - 1; i <= l + 1; i++) {
-//       if (i < 0 || i >= board.length) continue;
-//       for (var j = j - 1; j <= j + 1; j++) {
-//           if (i === i && j === j) continue;
-//           if (j < 0 || j >= board.length) continue;
-//           if (board[i][j] === MINE) neighborsCount++;
-//           console.log(neighborsCount);
-//       }
-//   }
-//   return neighborsCount;
-// }
+function isEmptyCell(coord){
+  return gBoard[coord.i][coord.j]===''
+}
+function isFirstMove(){
+  var cell = document.querySelectorAll('.hiden')
+  console.log(cell);
+  for (var i = 0; i < gBoard.length; i++) {
+    for (var j = 0; j < gBoard.length; j++) {
+      gBoard[i][j]
+    }
+  }
+}
 
-// function difficulty(btn){
-//   if(btn === 'easy'){
-//     gLevel.SIZE = 4
-//     gLevel.MINES = 4
-//   }
-//   if(btn === 'medium'){
-//     gLevel.SIZE = 6
-//     gLevel.MINES = 10
-//   }
-//   if(btn === 'hard'){
-//     gLevel.SIZE = 10
-//     gLevel.MINES = 15
-//   }
-// }
+function checkGameOver(cell) {
+  if (cell === gCell.isMine) {
+    console.log('You Lose');
+  }
+}
 
-// function cellClicked(elCell) {
-//   elCell.classList.remove('hiden')
-//   elCell.isShown = true
-//   getSelectedElCell = elCell
-//   var cellCoords = getSelectedCoord(elCell.id)
-//   checkGameOver(cellCoords)
-//   isFirstMove()
-// }
+function expandShown(board, elCell, i, j) { }
 
-// function getSelectedCoord(strCellId) {
-//   var coord = {}
-//   var parts = strCellId.split('-')
-//   coord.i = +parts[1]
-//   coord.j = +parts[2]
-//   return coord
-// }
-
-// function cellMarked(elCell) { }
-
-// function isEmptyCell(coord) {
-//   return gBoard[coord.i][coord.j] === ''
-// }
-
-// function isFirstMove() {
-//   var cell = document.querySelectorAll('.hiden')
-//   if(cell.length > 98){
-//     getMines(gLevel.MINES)
-//   }
-// }
-
-// function checkGameOver(cell) {
-//   if (cell === gCell.isMine) {
-//     console.log('You Lose');
-//   }
-// }
-
-// function expandShown(board, elCell, i, j) { }
-
-// function getMines(mine) {
-//   var mines = []
-//   for (var i = 0; i < gLevel.MINES; i++) {
-//     var cellI = getRandomInt(0, mine - 1)
-//     var cellJ = getRandomInt(0, mine - 1)
-//     var tempArr = [cellI, cellJ]
-//     if (!mines.includes(tempArr)) {
-//       mines.push(tempArr)
-//     }
-//   }
-//   return mines
-// }
+function getMines(mine) {
+  var mines = []
+  for (var i = 0; i < gLevel.MINES; i++) {
+    var cellI = getRandomInt(0, mine - 1)
+    var cellJ = getRandomInt(0, mine - 1)
+    var tempArr = [cellI, cellJ]
+    if (!mines.includes(tempArr)) {
+      mines.push(tempArr)
+    }
+  }
+  return mines
+}
--- sctipt/utils.js
+++ sctipt/utils.js
@@ -1,45 +1,49 @@
-function buildBoard() {
+function createBoard() {
   var board = []
-  var cell = gCell
   for (var i = 0; i < gLevel.SIZE; i++) {
     board[i] = []
     for (var j = 0; j < gLevel.SIZE; j++) {
-      board[i][j] = cell.minesAroundCount
+      board[i][j] = cell = {
+        minesAroundCount: 4,
+        isShown: false,
+        isMine: false,
+        isMarked: false
+      }
     }
   }
-  board[2][1] = MINE
-  board[3][2] = MINE
-  cell.minesAroundCount = setMinesNegsCount(board)
+  board[0][0]=MINE
+  board[2][2]=MINE
+  setMinesNegsCount(board)
   console.table(board)
   return board
 }
 
 function setMinesNegsCount(board) {
-  console.log(board.length);
-  
-  var neighborsCount = 0;
+  var newBoard = copyMat(board)
   for (var i = 0; i < board.length; i++) {
-    if(i < 0 || i > board.length-1)continue
     for (var j = 0; j < board.length; j++) {
-      if(j < 0 || j > board.length-1)continue
-      if(board[i][j]===MINE)continue
-      if(i===j)continue
+      var numOfNeighbors = counntNeigbors(i, j, board)
+      console.log('cell',i,j,numOfNeighbors);
+
     }
   }
-  return neighborsCount;
 }
+
 
 function renderBoard(board) {
   var strHtml = ''
   for (var i = 0; i < board.length; i++) {
     strHtml += '<tr>'
     for (var j = 0; j < board.length; j++) {
-      var currCell = board[i][j]
-      //var currClass = getClassName({i:i,j:j})
-      // var tdId = `cell-${i}-${j}`
-      // strHtml += `<td id="${tdId}" onclick="cellClicked(this)">`
-      // strHtml += currCell
-      // strHtml += `</td>`
+      var tdId = `cell-${i}-${j}`
+      strHtml += `<td id="${tdId}" onclick="cellClicked(this)">`
+      if (board[i][j].isMine) {
+        if (board[i][j].isShown) {
+          board[i][j].isShown = false
+          strHtml += MINE
+        }
+      }
+      strHtml += `</td>`
     }
     strHtml += '</tr>'
   }
@@ -47,6 +51,40 @@ function renderBoard(board) {
   elMat.innerHTML = strHtml
 }
 
+
+
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function copyMat(mat) {
+  var newMat = []
+  for (var i = 0; i < mat.length; i++) {
+    newMat[i] = []
+    for (var j = 0; j < mat.length; j++) {
+      newMat[i][j] = mat[i][j]
+    }
+  }
+  return newMat
+}
+
+function counntNeigbors(cellI, cellJ, mat) {
+  var neighbors = 0
+  for (var i = cellI - 1; i <= cellI + 1; i++) {
+    if (i < 0 || i >= mat.length) continue
+    for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+      if (i === cellI && j === cellJ) continue
+      if (j < 0 || j > mat.length) continue
+      if (mat[cellI]-1[cellJ]-1===MINE)neighbors++
+      if (mat[cellI][cellJ]===MINE)neighbors++
+      if (mat[cellI]+1[cellJ]+1===MINE)neighbors++
+      if (mat[cellI][cellJ]-1===MINE)neighbors++
+      if (mat[cellI][cellJ+1]===MINE)neighbors++
+      if (mat[cellI]+1[cellJ]-1===MINE)neighbors++
+      if (mat[cellI]+1[cellJ]===MINE)neighbors++
+      if (mat[cellI]+1[cellJ]+1===MINE)neighbors++
+      
+    }
+  }
+  return neighbors
 }
